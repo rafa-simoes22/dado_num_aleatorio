d4371c7766a0908889e35be0bde54c137381a160
Rafa <rafaelasimoes2203@mail.com>
2023-08-07 17:26:54 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dadinho Mágico',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: RandomNumberScreen(),
+    );
+  }
+}
+
+class RandomNumberScreen extends StatefulWidget {
+  @override
+  _RandomNumberScreenState createState() => _RandomNumberScreenState();
+}
+
+class _RandomNumberScreenState extends State<RandomNumberScreen> {
+  final Random _random = Random();
+  int _numeroSorteado = 1;
+
+  void _gerarNumeroAleatorio() {
+    setState(() {
+      _numeroSorteado = _random.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dadinho Mágico'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Número sorteado:',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '$_numeroSorteado',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _gerarNumeroAleatorio,
+              child: Text('Gerar Número'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
